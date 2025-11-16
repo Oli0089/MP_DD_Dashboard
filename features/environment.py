44f8edb8203ad0a53db029b8bb9286e799783391
Oli0089 <oliverkeeys@gmail.com>
2025-11-16 19:40:42 +0000

    Updated test env file so DB is created

--- features/environment.py
+++ features/environment.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app, db
 
 
 def before_scenario(context, scenario):
@@ -8,6 +8,10 @@ def before_scenario(context, scenario):
     # Behave needs CSRF off or it cannot POST to /login
     app.config["TESTING"] = True
     app.config["WTF_CSRF_ENABLED"] = False
+
+    # Ensure tables exist for Behave tests
+    with app.app_context():
+        db.create_all()
 
     # Provide a test client to all steps
     context.client = app.test_client()
